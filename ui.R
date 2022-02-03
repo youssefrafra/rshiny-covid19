996bc6232f7f7d40eba7c0a177dff58301c4695f
Thomas Caroff <83708180+Thomas-Caroff@users.noreply.github.com>
2022-02-03 13:41:29 +0100

    Fix bad merging
    Forgot ',' on line 74. Fixed

--- ui.R
+++ ui.R
@@ -71,7 +71,7 @@ shinyUI(dashboardPage(
                   h1('Top 10 Countries with most Covid-19 Cases'),
                   plotlyOutput("plot1"),
                   h1('Top 10 countries with most vaccinated population'),
-                  plotlyOutput("plot2")
+                  plotlyOutput("plot2"),
                   h1('Top 10 countries with highest percent of vaccinated population'),
                   plotlyOutput("plot3")
           ), # close tabItem
